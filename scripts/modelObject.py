d827b3e324902760f8c0a91e73596492ad02b41e
erinmaley <126917906+erinmaley@users.noreply.github.com>
2023-04-20 09:24:23 -0500

    Update modelObject.py

--- scripts/modelObject.py
+++ scripts/modelObject.py
@@ -1,10 +1,13 @@
+# Import necessary module
 import pickle
 
+# Define a function called store that takes in a trained model and a file path as inputs
 def store(model, filepath):
     with open(filepath, 'wb') as file:
         pickle.dump(model, file)
     print(f'Model stored to the following path: {filepath}')
 
+# Define a function called 'openIt' that takes a file path as input    
 def openIt(filepath):
     with open(filepath, 'rb') as file:
         model = pickle.load(file)
