0ce9895ace1c37dca0b6d794cbcfb81cae35653d
moesabi <104253630+moesabi@users.noreply.github.com>
2023-04-24 16:01:01 -0500

    Update randomforest.py

--- scripts/randomforest.py
+++ scripts/randomforest.py
@@ -8,7 +8,8 @@ import json
 from modelObject import store
 from storePerformance import storeIt
 from accuracy import accuracy
-from accuracy import confusionMat
+from accuracy import confusionMat, roc
+from backwardselection import importances
 
 parser = argparse.ArgumentParser(description='Run Random Forest script')
 parser.add_argument('-i', '--input', help='input folder path for data', required=True)
@@ -18,6 +19,7 @@ parser.add_argument('-o', '--output', help='output pickle file path', required=T
 parser.add_argument('-r', '--results', help='path to results csv', required=True)
 parser.add_argument('-n', '--numProcessors', help='number of processers', required=False, default=None)
 parser.add_argument('-m', '--matrix', help='confusion matrix path', required=True)
+parser.add_argument('-a', '--curve', help='path for roc curve', required=False, default='outputROC.png')
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -38,9 +40,23 @@ if __name__ == '__main__':
 
     g = GridSearchCV(RandomForestClassifier(random_state=168), param_grid, refit=True, verbose=3, cv=int(args.crossfolds), n_jobs=int(args.numProcessors))
     g.fit(x_train, y_train)
-
+    
+    
     rf_classifier = g.best_estimator_
     store(rf_classifier, args.output)
-    confusionMat(rf_classifier, x_test, y_test, args.matrix)
-    acc = accuracy(rf_classifier, x_test, y_test)
-    storeIt('Random Forest', f'{g.best_params_}', acc, args.output, args.results)
+    
+    
+    # calculate AUC and highest threshold
+    area, threshold = roc(rf_classifier, x_test, y_test, args.curve)
+
+    
+    # create a confusion matrix and store it
+    confusionMat(rf_classifier, x_test, y_test, args.matrix, threshold)
+
+    # calculate F1 score and accuracy
+    f1, acc = accuracy(rf_classifier, x_test, y_test, threshold)
+
+    # calculate and store variable importances
+    variable_importances = importances(rf_classifier, x_test, y_test).to_dict()
+
+    storeIt('Random Forest', f'{g.best_params_}', {'AUC': area, 'f1score': f1, 'regularAccuracy': acc}, args.output, args.results, variable_importances)
